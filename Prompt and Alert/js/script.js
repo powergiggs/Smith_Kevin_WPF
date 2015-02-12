8383a5dd8d4eb5a70a883044b61f9334b336ad26
powergiggs <kevin@powergiggs.com>
2015-02-12 18:03:29 -0500

    Test code
    Print test to console

--- Prompt and Alert/js/script.js
+++ Prompt and Alert/js/script.js
@@ -3,10 +3,11 @@
 /*---Prompt and Alert--*/
 
 
-var width = prompt("We are calculating the area of rectangle. Please enter width");
+var width = prompt("We are calculating the area of rectangle.\nPlease enter width");
 var height = prompt("Please enter height");
+var area = width * height;//calculated
 
 
 
-//console.log(userInput);
+console.log(area);
 
