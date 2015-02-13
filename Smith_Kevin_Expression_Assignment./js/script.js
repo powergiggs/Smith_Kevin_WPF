94a6070621c436034a4650ce6434703f59d82c21
powergiggs <kevin@powergiggs.com>
2015-02-13 13:20:55 -0500

    Calculation
    calculation done and print to console.log for accuracy

--- Smith_Kevin_Expression_Assignment./js/script.js
+++ Smith_Kevin_Expression_Assignment./js/script.js
@@ -1,10 +1,18 @@
 /**  Kevin Smith, 2/13/15, SDI_Expression_Assignment */
 
-var dayOne = prompt ("We are calculating your average daily miles run for 3 days. \n Please enter miles "); // Prompt user for the first  input value
+var dayOne = prompt ("We are calculating your average daily miles run for 5 days. \n Please enter miles "); // Prompt user for the first  input value
 var dayTwo = prompt ("Please enter the miles.");   //prompt user for the second input value
 
-var dayThree = prompt ("Please enter miles one more time."); // prompt user for the third input value
+var dayThree = prompt ("Please enter the miles."); // prompt user for the third input value
 
-var arrayValues = [dayOne, dayTwo, dayThree];   //
+var dayFour = prompt ("Please enter the miles."); // prompt user for fourth input value
 
- console.log(arrayValues);
+var dayFive = prompt ("Please enter final miles.");   // prompt user for final input value
+
+//var arrayValues = Number [dayOne, dayTwo, dayThree, dayFour, dayFive];   // array variable to store the date from user
+
+//var result =  (arrayValues.) /5;
+
+var average = (Number(dayOne)+Number(dayTwo)+Number(dayThree)+Number(dayFour)+Number(dayFive))/5; //   Variable use to store calculation for the average result
+
+ console.log(average);
