6ac19327f9093fb386ad96610886fc1062107a7a
Kevin Smith <kevin@powergiggs.com>
2015-02-12 22:00:38 -0500

    Slice of Pie part 1
    variables declare for given and variable for calculations
    result print to console.log

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -5,10 +5,16 @@
 var humanAge = 2; // define variable for Sparky age
 
 var dogAge = humanAge * 7; //  calculations for sparky human age
-
 console.log ("Sparky is " + humanAge + " human years old which is "+ dogAge +" in dog years" +"." ); // printing Sparky human and dog age result
 
 /**Slice of Pie part 1 */
 
+var pizzaSlice = 10; // variable for number of slices per pizza
 
+var nuPeople = 20; // Variable for number of people at party
 
+var nuPizzaOrder = 20; // variable to represent number of pizzas ordered
+
+var slicePerPerson = (pizzaSlice * nuPizzaOrder)/nuPeople; // calculations for number of pizza slices per person
+
+console.log (slicePerPerson);
