ed59d9bcb8ea9053a02ca6424614bab89b685af3
powergiggs <kevin@powergiggs.com>
2015-02-13 11:27:28 -0500

    Expression Assignment
    variables set for user prompt input and print to console

--- Smith_Kevin_Expression_Assignment./js/script.js
+++ Smith_Kevin_Expression_Assignment./js/script.js
@@ -1,1 +1,10 @@
 /**  Kevin Smith, 2/13/15, SDI_Expression_Assignment */
+
+var dayOne = prompt ("We are calculating your average daily miles run for 3 days. \n Please enter miles "); // Prompt user for the first  input value
+var dayTwo = prompt ("Please enter the miles.");   //prompt user for the second input value
+
+var dayThree = prompt ("Please enter miles one more time."); // prompt user for the third input value
+
+
+
+ console.log(dayThree);
