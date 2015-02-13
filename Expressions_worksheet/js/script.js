7f51ca74f99b79f67dbdc0ba53296ad7e7a6b34c
Kevin Smith <kevin@powergiggs.com>
2015-02-12 21:11:54 -0500

    Dog years
    Create variable and calculation for dog and dog human age

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,3 +1,10 @@
 /** Kevin Smith--02/12/2015--Expression Worksheet */
 
+/**Dog Years*/
+
+var sparkyAge = 2; // define variable for Sparky age
+
+var humanAge = sparkyAge * 7; // define variable for calculation for sparky human age
+
+console.log ("Sparky is " + humanAge + " human years old which is "+ sparkyAge +" in dog years" +"." ); // testing Sparky human gae result
 
