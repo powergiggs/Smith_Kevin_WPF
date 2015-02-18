8533f58f62cf8fdd7703341f6f1472f7806c2640
powergiggs <kevin@powergiggs.com>
2015-02-17 20:47:01 -0500

    Condition with expressions
    adding expression to conditions

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -2,14 +2,18 @@
  * Created by kevinsmith on 2/17/15.
  */
 
-var kidHeight = 40;
+    //Conditional logic
+
+var kidHeight = 47;
 var minHeight = 48;
+var sneakerLift = 2;
+
 
 //if the child is old enough print to console "you can ride"
 //if the kid is over 48 inches in height
 
-if(kidHeight > minHeight ) {
+if(kidHeight + sneakerLift > minHeight ) {
         // code perform if condition is true
     console.log("You can ride the coaster!");
 }
-console.log ("What comes after");
+//console.log ("What comes after");
