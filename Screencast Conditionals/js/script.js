c12413278018812f6f4dcaffe34c48e91cd78767
powergiggs <kevin@powergiggs.com>
2015-02-17 21:03:29 -0500

    if and else
    using if and else in condition

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -4,16 +4,16 @@
 
     //Conditional logic
 
-var kidHeight = 47;
+var kidHeight = 50;
 var minHeight = 48;
-var sneakerLift = 2;
-
 
 //if the child is old enough print to console "you can ride"
 //if the kid is over 48 inches in height
 
-if(kidHeight + sneakerLift > minHeight ) {
+if(kidHeight > minHeight ) {
         // code perform if condition is true
     console.log("You can ride the coaster!");
+}else{
+        //code perform if condition is false
+    console.log ("Sorry kid! you got some growing to do first");
 }
-//console.log ("What comes after");
