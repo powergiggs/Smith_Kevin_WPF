f3c45061f9be0ca34c56ca4f908bbdd505fe9ca8
powergiggs <kevin@powergiggs.com>
2015-02-17 20:30:09 -0500

    conditional
    example conditional

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -2,4 +2,12 @@
  * Created by kevinsmith on 2/17/15.
  */
 
-alert("Hello World!!");
+var oldEnough = false;
+
+//if the child is old enough print to console "you can ride"
+
+if(oldEnough) {
+        // code perform if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log ("What comes after");
