21c30be38e710646bc1422f0fb0ffddfe9806266
powergiggs <kevin@powergiggs.com>
2015-02-17 21:20:53 -0500

    Else If
    Using else if with three conditions

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -2,18 +2,24 @@
  * Created by kevinsmith on 2/17/15.
  */
 
-    //Conditional logic
+    //Conditional logic else if
 
-var kidHeight = 50;
+var kidHeight = 46;
 var minHeight = 48;
+var wParentHeight = 45;
 
 //if the child is old enough print to console "you can ride"
 //if the kid is over 48 inches in height
 
 if(kidHeight > minHeight ) {
-        // code perform if condition is true
+        // you can ride
     console.log("You can ride the coaster!");
-}else{
+
+}else if (kidHeight > wParentHeight){
         //code perform if condition is false
+    console.log ("you can ride, but only with parent present");
+}else{
+
+
     console.log ("Sorry kid! you got some growing to do first");
 }
