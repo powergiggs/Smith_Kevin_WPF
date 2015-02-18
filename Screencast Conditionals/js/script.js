c26109d9b59e0f85710894c6f2c8b46b036c933a
powergiggs <kevin@powergiggs.com>
2015-02-17 20:39:07 -0500

    Conditionals
    comparing two values

--- Screencast Conditionals/js/script.js
+++ Screencast Conditionals/js/script.js
@@ -2,11 +2,13 @@
  * Created by kevinsmith on 2/17/15.
  */
 
-var oldEnough = false;
+var kidHeight = 40;
+var minHeight = 48;
 
 //if the child is old enough print to console "you can ride"
+//if the kid is over 48 inches in height
 
-if(oldEnough) {
+if(kidHeight > minHeight ) {
         // code perform if condition is true
     console.log("You can ride the coaster!");
 }
