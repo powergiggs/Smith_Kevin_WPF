c8614eb7598f1bdccc51aa22b58e81ae6fa9122a
powergiggs <kevin@powergiggs.com>
2015-02-19 13:06:57 -0500

    Ternary operators
    folders and files created

---
+++ Screencast Conditionals/Ternary Operators/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by kevinsmith on 2/19/15.
+ */
+
+//Ternary Operators
+
+var gpa = 48;
+
+// if gpa is over the min score, the student can graduate
+
+
+if(gpa > 2.0){
+    console.log("You can graduate!");
+
+
+}else{
+    console.log("GPA is too low!");
+
+}
