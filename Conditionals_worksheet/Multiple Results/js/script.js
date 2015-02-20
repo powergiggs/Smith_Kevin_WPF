05f74a3fe6b2c60c335b32a9c86d2323d94e3388
powergiggs <kevin@powergiggs.com>
2015-02-19 22:41:05 -0500

    conditional worksheet-multiple result
    all the conditions are met and tested

--- Conditionals_worksheet/Multiple Results/js/script.js
+++ Conditionals_worksheet/Multiple Results/js/script.js
@@ -7,24 +7,27 @@
  * Check the Login
  */
 
-var enterName = prompt("Please enter your user name"); // prompt user for name
+var enterName = prompt("Please enter your user name"); // prompt user for user name
 var enterPassword = prompt("Please enter the correct password"); // prompt user for password
 
 var userName = "powergiggs"; // variable storing username
 var passWord = "happy"; // variable storing password
 
-// if the user name and password is equal to the store password print result
+// if the user name and password is equal to the variable user and  password print to console
 
 if(enterName === userName && enterPassword === passWord ){
 
-    console.log("Welcome," +userName+ "!"); // code execute if the condition true
+    console.log("Welcome," +userName+ "!"); // code execute if the condition is true
 
 
     }else if(enterName != userName ){
 
-    console.log("User not found. Try again.")
+    console.log("User not found. Try again.") // code execute if user name do not match
 
 
+    }else if(enterName === userName && enterPassword != passWord ){
+
+    console.log("Password does not match our records.") // code execute if username match but password doesn't
 }
 
 //console.log(enterName);
