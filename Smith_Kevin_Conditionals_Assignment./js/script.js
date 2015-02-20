cd077c67b9682aa09cc4c8da45a18590818c5259
powergiggs <kevin@powergiggs.com>
2015-02-20 03:33:35 -0500

    Conditionals Assignment
    conditions declare and tested

--- Smith_Kevin_Conditionals_Assignment./js/script.js
+++ Smith_Kevin_Conditionals_Assignment./js/script.js
@@ -10,8 +10,25 @@ var weeklyHours = prompt("Please enter your Weekly Hours");// prompt user to ent
 var hourRate = prompt ("Please enter your hourly rate"); // prompt user to enter their hourly rate
 var incomeTax = prompt ("Please enter your income tax rate"); // prompt user to enter their income tax rate
 
+var wkSalary = weeklyHours * hourRate; //calculation for salary before tax
+
+var taxSalary = wkSalary - incomeTax/100 * wkSalary ;// salary with tax deduction
+
+if(weeklyHours === ""  && hourRate === "" ){ //codes execute if there is no value enter in  prompt
+
+    console.log ("please enter values for weekly hours and hourly rate");
+
+
+    }else if(incomeTax === ""){
+
+    console.log("Please enter Income tax rate");
+
+}else{
+
+ conso
+
+}
 
 
 
-
-console.log (weeklyHours);
+//console.log (taxSalary);
