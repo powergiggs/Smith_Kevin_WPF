a140e57e02282ece195f844972e636243b122602
powergiggs <kevin@powergiggs.com>
2015-02-19 19:42:14 -0500

    conditional worksheet
     if and else result print to console

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,12 +5,27 @@
  */
 
 var mpg = 30;
-var gasGauge = 44;
-var gasTankCapacity =20;
+var gasGauge = 50;
+var gasTankCapacity =15;
+
+//if the gas mileage per gallon and Gauge level  is more than the gas thank capacity
+
+//print to console Yes, you can make it without stopping for gas!
+
+if(gasGauge > mpg && mpg > gasTankCapacity ){
+
+    console.log("Yes, you can make it without stopping for gas!");
+    // code print to console if conditions is true
+
+
+}else{
+
+    console.log("You only have " + gasTankCapacity +" gallons of gas in your tank,\nbetter get gas now while you can! ");
+
+//code print to console if condition is falls
+
+}
 
 
 
 
-
-
-//alert("Hello World!!");
