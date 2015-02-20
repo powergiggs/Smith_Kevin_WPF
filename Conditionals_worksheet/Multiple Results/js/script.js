cbed0f8ba88c46cde94fa8bc83e01e5d7a0bebe0
powergiggs <kevin@powergiggs.com>
2015-02-19 21:44:23 -0500

    Multiple Result
    folders and files created

---
+++ Conditionals_worksheet/Multiple Results/js/script.js
@@ -0,0 +1,20 @@
+/**
+ *  Kevin Smith
+ * 2/19/15
+ *
+ * Conditionals Worksheet,
+ * Multiple Results
+ * Check the Login
+ */
+
+var enterName = prompt("Please enter your user name");
+var enterPassword = prompt("please enter the correct password");
+
+var userName = "powergiggs";
+var passWord = "happy";
+
+
+
+console.log(enterName);
+
+
