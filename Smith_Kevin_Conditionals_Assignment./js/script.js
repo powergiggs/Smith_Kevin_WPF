49339aa78e433db31b8cf1a113351b8a4d5f3d10
powergiggs <kevin@powergiggs.com>
2015-02-20 12:38:08 -0500

    Conditionals Assignment
    Ternary conditions added

--- Smith_Kevin_Conditionals_Assignment./js/script.js
+++ Smith_Kevin_Conditionals_Assignment./js/script.js
@@ -9,6 +9,7 @@
 var weeklyHours = prompt("Please enter your Weekly Hours");// prompt user to enter their total hours work
 var hourRate = prompt ("Please enter your hourly rate"); // prompt user to enter their hourly rate
 var incomeTax = prompt ("Please enter your income tax rate"); // prompt user to enter their income tax rate
+var overTime;
 
 var wkSalary = weeklyHours * hourRate; //calculation for salary before tax
 
@@ -18,18 +19,25 @@ var taxSalary = wkSalary - incomeTax/100 * wkSalary ;// salary with tax deductio
 
 if(weeklyHours === ""  && hourRate === "" ){
 
-    console.log ("please enter values for weekly hours and hourly rate"); //codes execute if there is no value enter in  prompt
+    console.log ("please enter values for weekly hours and hourly rate"); //codes execute if there is no value enter in for prompt
 
+//if the value equal true cods print to console
 
     }else if(incomeTax === ""){
 
-    console.log("Please enter Income tax rate");
+    console.log("Please enter Income tax rate");// code execute if no value is enter in prompt
 
 }else{
 
  console.log("your weekly salary before tax is $" +wkSalary + " and $"+ taxSalary + " after tax."  );
 
 }
+
+//if the weekly hours is more than 40 hours get "you have earn some extra cash" otherwise "No overtime sorry"
+
+overTime = (weeklyHours > 40) ? "You have earn some extra cash" : "No overtime sorry"; //
+
+console.log(overTime);
 
 
 
