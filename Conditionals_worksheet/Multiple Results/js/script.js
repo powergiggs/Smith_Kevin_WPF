ba8cf0820a6a89b423c70cea9d4bc2d58ceeebf4
powergiggs <kevin@powergiggs.com>
2015-02-19 22:25:40 -0500

    conditional worksheet-multiple results
    print to console if conditions

--- Conditionals_worksheet/Multiple Results/js/script.js
+++ Conditionals_worksheet/Multiple Results/js/script.js
@@ -13,7 +13,19 @@ var enterPassword = prompt("Please enter the correct password"); // prompt user 
 var userName = "powergiggs"; // variable storing username
 var passWord = "happy"; // variable storing password
 
+// if the user name and password is equal to the store password print result
 
+if(enterName === userName && enterPassword === passWord ){
+
+    console.log("Welcome," +userName+ "!"); // code execute if the condition true
+
+
+    }else if(enterName != userName ){
+
+    console.log("User not found. Try again.")
+
+
+}
 
 //console.log(enterName);
 
