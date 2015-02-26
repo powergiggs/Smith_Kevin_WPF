14510c7c7ac6487360341737aaedbc7ecdbd2020
powergiggs <kevin@powergiggs.com>
2015-02-26 01:43:46 -0500

    Function worksheet
    folders and files created
    function created for the calculation of circumference

---
+++ Functions_worksheet/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Kevin Smith on
+ * 2/26/1
+ * Functions_worksheet
+ */
+
+
+//Calculate the circumference of a circle.
+
+var cirTotal = calcCircumference (4); //variable to store function
+
+function calcCircumference (radius){
+    var circumference = 2 * 3.14 * radius; // code for circumference calculation
+
+    return circumference; // return value outside function
+}
+
+//console.log(cirTotal);
