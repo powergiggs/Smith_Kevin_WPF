de50cc3b1ca71098debd4d65d5becdc30d6b8d20
powergiggs <kevin@powergiggs.com>
2015-02-26 01:51:23 -0500

    Function Worksheet
    print to console.log  to check code

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,12 +7,12 @@
 
 //Calculate the circumference of a circle.
 
-var cirTotal = calcCircumference (4); //variable to store function
+var circumTotal = calculateCircumference (4); //variable to store function
 
-function calcCircumference (radius){
-    var circumference = 2 * 3.14 * radius; // code for circumference calculation
+function calculateCircumference (radius){
+    var circumference = 2 * Math.PI * radius; // code for circumference calculation
 
     return circumference; // return value outside function
 }
 
-//console.log(cirTotal);
+console.log(circumTotal);
