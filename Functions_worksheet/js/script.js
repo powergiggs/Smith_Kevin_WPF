05d33c9d5effe69fd1abce33b7b6a11c9a1592c1
powergiggs <kevin@powergiggs.com>
2015-02-26 02:05:23 -0500

    Functions worksheet- Circumference
    change the parameters of radius to ensure function calculation is
    printing the correct result to console.log

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -7,7 +7,7 @@
 
 //Calculate the circumference of a circle.
 
-var circumTotal = calculateCircumference (8); //variable to store function
+var circumTotal = calculateCircumference (78); //variable to store function
 
 function calculateCircumference (radius){
     var circumference = 2 * Math.PI * radius; // code for circumference calculation
