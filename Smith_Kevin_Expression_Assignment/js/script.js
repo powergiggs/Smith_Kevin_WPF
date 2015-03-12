b2da7cad0a99a06b8cf2d5e784759ae5b352e5fa
powergiggs <kevin@powergiggs.com>
2015-03-12 16:37:58 -0400

    expression assigment
    variables for prompt declare

--- Smith_Kevin_Expression_Assignment/js/script.js
+++ Smith_Kevin_Expression_Assignment/js/script.js
@@ -5,4 +5,12 @@
 
  */
 
-alert("File Link");
+
+var principalAmount = prompt("Please enter the amount of money borrow"); // Prompt user to enter the principal amount being borrowed
+
+var rate = prompt ("Please enter the percentage rate for loan"); // prompt user to enter the percentage rate charge for loan
+
+var timePeriod = prompt ("Please enter time period in months for\n which the money is being borrowed"); // prompt user to enter the time period for loan in months
+
+
+console.log(principalAmount);
