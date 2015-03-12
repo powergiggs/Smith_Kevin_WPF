68b3f94f4aea581119d5f814fd8365333c073fae
powergiggs <kevin@powergiggs.com>
2015-03-12 17:36:14 -0400

    Expression Assignment
    variables declare

--- Smith_Kevin_Expression_Assignment/js/script.js
+++ Smith_Kevin_Expression_Assignment/js/script.js
@@ -6,11 +6,19 @@
  */
 
 
-var principalAmount = prompt("Please enter the amount of money borrow"); // Prompt user to enter the principal amount being borrowed
+var principalAmount =  prompt("Please enter the amount of money borrow"); // Prompt user to enter the principal amount being borrowed
 
-var rate = prompt ("Please enter the percentage rate for loan"); // prompt user to enter the percentage rate charge for loan
+var rate =  prompt ("Please enter the percentage rate for loan"); // prompt user to enter the percentage rate charge for loan
 
-var timePeriod = prompt ("Please enter time period in months for\n which the money is being borrowed"); // prompt user to enter the time period for loan in months
+var timePeriod = prompt ("Please enter time period in years for\n which the money is being borrowed"); // prompt user to enter the time period for loan in months
+
+rate = rate/100; //calculation for reducing rate variable to decimal
+
+var userValue =  [Number(principalAmount), Number(timePeriod)];// array variable for converting user input to number value
 
 
-console.log(principalAmount);
+
+
+
+
+console.log(userValue);
