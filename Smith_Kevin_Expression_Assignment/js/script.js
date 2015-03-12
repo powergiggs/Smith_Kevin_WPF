19c83e013533d8994cc53016dce7725ee9e93f0e
powergiggs <kevin@powergiggs.com>
2015-03-12 18:20:19 -0400

    expression assignment
    print calculation of input result to console.log

--- Smith_Kevin_Expression_Assignment/js/script.js
+++ Smith_Kevin_Expression_Assignment/js/script.js
@@ -8,17 +8,20 @@
 
 var principalAmount =  prompt("Please enter the amount of money borrow"); // Prompt user to enter the principal amount being borrowed
 
-var rate =  prompt ("Please enter the percentage rate for loan"); // prompt user to enter the percentage rate charge for loan
+var rate =  prompt ("Please enter the percentage(%) rate for loan"); // prompt user to enter the percentage rate charge for loan
 
 var timePeriod = prompt ("Please enter time period in years for\n which the money is being borrowed"); // prompt user to enter the time period for loan in months
 
-rate = rate/100; //calculation for reducing rate variable to decimal
+//rate = rate/100; //calculation for reducing rate variable to decimal
 
-var userValue =  [Number(principalAmount), Number(rate), Number(timePeriod)];// array variable for converting user input to number value
+var userValue =  [Number(principalAmount), Number(rate), Number(timePeriod)];// array variable for storing user input  and converting string to number value
 
-var interestCalc = userValue[0] * rate * userValue [1];// variable to calculate and store result from user prompt input
-
-
+var interestCalc = userValue[0] * (userValue[1]/100) * userValue [2];// variable to calculate  result from user  input
 
 
-//console.log(interestCalc.toFixed());
+
+
+console.log("Hello your interest amount is $" + interestCalc.toFixed()+ " for your loan"); // print calculation result to console.log
+
+
+
