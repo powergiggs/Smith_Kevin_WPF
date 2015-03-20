b075a1c46451f5834195d8695ca77ac48859ad68
powergiggs <kevin@powergiggs.com>
2015-03-19 21:35:46 -0400

    conditionals assignment
    add if else conditions and print to console

--- Smith_Kevin_Conditionals_Assignment/js/script.js
+++ Smith_Kevin_Conditionals_Assignment/js/script.js
@@ -12,6 +12,22 @@ var fuelPrice = prompt("Please enter the cost of fuel per gallon"); // prompt us
 var fuelVolume = numberMiles/milesPerGallon;// variable to calculate fuel volume
 var tripCost = fuelVolume * fuelPrice; // variable to calculate trip cost for fuel
 
+if(numberMiles === "" && milesPerGallon === ""  ){
+
+    console.log ("Please enter a  value for miles and miles per gallon");// print to console if condition is true
+
+} else if(fuelPrice === ""){
+
+    console.log("Please enter fuel cost"); // print to console if condition 1 is false and condition is true
 
 
-console.log(tripCost);
+}else{
+
+    console.log ("Your " + numberMiles + " miles road trip will cost you $" + tripCost.toFixed(2)+ " in fuel");// 
+}
+
+
+
+
+
+//console.log(numberMiles);
