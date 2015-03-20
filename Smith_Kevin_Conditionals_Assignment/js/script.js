ca9ce721238d86ebeed58825eb67f2b5a1bfcb19
powergiggs <kevin@powergiggs.com>
2015-03-19 22:18:55 -0400

    conditionals assignment
    ternary statement added

--- Smith_Kevin_Conditionals_Assignment/js/script.js
+++ Smith_Kevin_Conditionals_Assignment/js/script.js
@@ -11,23 +11,25 @@ var fuelPrice = prompt("Please enter the cost of fuel per gallon"); // prompt us
 
 var fuelVolume = numberMiles/milesPerGallon;// variable to calculate fuel volume
 var tripCost = fuelVolume * fuelPrice; // variable to calculate trip cost for fuel
+var costlyTrip;
 
-if(numberMiles === "" && milesPerGallon === ""  ){
+
+if(numberMiles === "" && milesPerGallon === ""  ){// if condition is true
 
     console.log ("Please enter a  value for miles and miles per gallon");// print to console if condition is true
 
-} else if(fuelPrice === ""){
+} else if(fuelPrice === ""){ // if the condition is true
 
     console.log("Please enter fuel cost"); // print to console if condition 1 is false and condition is true
 
 
 }else{
 
-    console.log ("Your " + numberMiles + " miles road trip will cost you $" + tripCost.toFixed(2)+ " in fuel");// 
+    console.log ("Your " + numberMiles + " miles road trip will cost you $" + tripCost.toFixed(2)+ " in fuel at " + milesPerGallon + " miles per gallon. ");// print to console if condition 1 and 2 is false
 }
 
+costlyTrip = (tripCost >= 30) ? "Very expensive trip costing you $30 or more" :"Not an expensive trip costing you less than $30"; // 
 
 
 
-
-//console.log(numberMiles);
+console.log(costlyTrip); pr
